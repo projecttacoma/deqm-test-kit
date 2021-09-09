1c05050cab51110c48204e3c3ab2f167130141f9
natjoe4 <njjones@mitre.org>
2021-09-09 14:35:23 -0400

    working dr tests

--- lib/deqm_test_kit/data_requirements.rb
+++ lib/deqm_test_kit/data_requirements.rb
@@ -9,29 +9,59 @@ module DEQMTestKit
       fhir_client do
         url :url
       end
+
+      fhir_client :embedded_client do
+        url 'http://cqf_ruler:8080/cqf-ruler-r4/fhir'
+      end
   
       test do
         title 'Check data requirements against expected return'
         id 'data-requirements-01'
         description 'Data requirements on the fhir test server match the data requirements of our embedded client'
         makes_request :data_requirements
-  
+
+        PARAMS = {
+            resourceType: "Parameters",
+            parameter:[{
+                periodStart: '2019-01-01',
+                periodEnd: '2019-12-31'
+              
+            }]
+      }
         run do
           # Look for matching measure from cqf-ruler datastore by resource id
           # TODO: actually pull measure from user input drop down (populated from embedded client)
           measure_to_test = 'EXM130|7.3.000'
           measure_identifier, measure_version = measure_to_test.split('|')
-  
-          # @client.additional_headers = { 'x-api-key': @instance.api_key, 'Authorization': @instance.auth_header } if @instance.api_key && @instance.auth_header
-  
+    
           # Search system for measure by identifier and version
           fhir_search(:measure, params: { name: measure_identifier, version: measure_version }, name: :measure_search)
-          measure_bundle = JSON.parse(response[:body])
-          id = measure_bundle.entry[0].resource.id;
-          fhir_operation("Measure/#{id}/$data-requirements", name: :data_requirements)
           assert_response_status(200)
-          assert_resource_type(:measure)
+          assert_resource_type(:bundle)
           assert_valid_json(response[:body])
+          measure_bundle = JSON.parse(response[:body])
+          assert measure_bundle["total"].positive?, "Expected to find measure with identifier #{measure_identifier} and version #{measure_version}"
+          
+          id = measure_bundle["entry"][0]["resource"]["id"]
+
+          fhir_operation("Measure/#{id}/$data-requirements", body: PARAMS, name: :data_requirements)
+          library = JSON.parse(response[:body])
+          actual_dr = library["dataRequirement"]
+          assert_response_status(200)
+          assert_resource_type(:library)
+          assert_valid_json(response[:body])
+
+          fhir_search(:measure, client: :embedded_client, params: { name: measure_identifier, version: measure_version }, name: :measure_search)
+          measure_bundle = JSON.parse(response[:body])
+          
+          id = measure_bundle["entry"][0]["resource"]["id"]
+          
+          fhir_operation("Measure/#{id}/$data-requirements", body: PARAMS, client: :embedded_client, name: :data_requirements)
+          expected_library = JSON.parse(response[:body])
+          expected_dr = expected_library["dataRequirement"]
+
+          assert((expected_dr - actual_dr).blank?, "Client data-requirements is missing expected data requirements for measure #{id}")
+          assert((actual_dr - expected_dr).blank?, "Client data-requirements contains unexpected data requirements for measure #{id}")
 
         end
       end
