e19dcaa40c34369cc02beeca24f7bb2e96358c48
natjoe4 <njjones@mitre.org>
2021-11-02 12:22:28 -0400

    added negative test cases to data-requirements suite

--- lib/deqm_test_kit/data_requirements.rb
+++ lib/deqm_test_kit/data_requirements.rb
@@ -24,16 +24,17 @@ module DEQMTestKit
       parameter: [{
         periodStart: '2019-01-01',
         periodEnd: '2019-12-31'
-
       }]
     }.freeze
 
-    PARAMS = {
+    MISSING_PARAMS = {
       resourceType: 'Parameters',
       parameter: [{
         periodStart: '2019-01-01'
       }]
     }.freeze
+
+    INVALID_ID = 'INVALID_ID'
 
     # rubocop:disable Metrics/BlockLength
     test do
@@ -54,7 +55,8 @@ module DEQMTestKit
         measure_version = resource.version
 
         # Run our data requirements operation on the test client server
-        fhir_operation("Measure/#{measure_id}/$data-requirements", body: PARAMS, name: :data_requirements)
+        fhir_operation("Measure/#{measure_id}/$data-requirements?periodEnd=2019-12-31&periodStart=2019-01-01",
+                       body: PARAMS, name: :data_requirements)
         assert_response_status(200)
         assert_resource_type(:library)
         assert_valid_json(response[:body])
@@ -69,7 +71,11 @@ module DEQMTestKit
         embedded_client_id = resource.entry[0].resource.id
 
         # Run data requirements operation on embedded cqf-ruler instance
-        fhir_operation("Measure/#{embedded_client_id}/$data-requirements", body: PARAMS, client: :embedded_client)
+        fhir_operation(
+          "Measure/#{embedded_client_id}/$data-requirements?periodEnd=2019-12-31&periodStart=2019-01-01",
+          body: PARAMS,
+          client: :embedded_client
+        )
         expected_dr = resource.dataRequirement
 
         expected_dr_strings = get_dr_comparison_list expected_dr
@@ -91,57 +97,34 @@ module DEQMTestKit
       end
     end
     test do
-      title 'Check data requirements against expected return'
+      title 'Check data requirements returns 400 for missing parameters'
       id 'data-requirements-02'
-      description 'Data requirements fails with missing parameters'
-      makes_request :data_requirements
-      output :queries_json
-      input :measure_id
+      description 'Data requirements returns 400 when periodStart and periodEnd parameters are omitted'
 
       run do
-        # Get measure resource from client
-        fhir_read(:measure, measure_id)
-        assert_response_status(200)
-        assert_resource_type(:measure)
-        assert_valid_json(response[:body])
-        measure_identifier = resource.name
-        measure_version = resource.version
-
         # Run our data requirements operation on the test client server
-        fhir_operation("Measure/#{measure_id}/$data-requirements", body: MISSING_PARAMS)
-        assert_response_status(200)
-        assert_resource_type(:library)
+        fhir_operation("Measure/#{INVALID_ID}/$data-requirements", body: MISSING_PARAMS)
+        assert_response_status(400)
         assert_valid_json(response[:body])
+        assert(resource.resourceType == 'OperationOutcome')
+        assert(resource.issue[0].severity == 'error')
+      end
+    end
+    test do
+      title 'Check data requirements returns 400 for invalid measure id'
+      id 'data-requirements-03'
+      description 'Data requirements returns 400 when passed a measure id which is not in the system'
 
-        actual_dr = resource.dataRequirement
-
-        actual_dr_strings = get_dr_comparison_list actual_dr
-
-        # Search embedded cqf-ruler instance by identifier and version
-        fhir_search(:measure, client: :embedded_client,
-                              params: { name: measure_identifier, version: measure_version })
-        embedded_client_id = resource.entry[0].resource.id
-
-        # Run data requirements operation on embedded cqf-ruler instance
-        fhir_operation("Measure/#{embedded_client_id}/$data-requirements", body: PARAMS, client: :embedded_client)
-        expected_dr = resource.dataRequirement
-
-        expected_dr_strings = get_dr_comparison_list expected_dr
-
-        diff = expected_dr_strings - actual_dr_strings
-
-        # still output queries even if different from expected.
-        # TODO: output queries only if pass once they align with cqf-ruler
-        queries = get_data_requirements_queries(actual_dr)
-        output queries_json: queries.to_json
-
-        # Ensure both data requirements results libraries are identical
-        assert(diff.blank?,
-               "Client data-requirements is missing expected data requirements for measure #{measure_id}: #{diff}")
-
-        diff = actual_dr_strings - expected_dr_strings
-        assert(diff.blank?,
-               "Client data-requirements contains unexpected data requirements for measure #{measure_id}: #{diff}")
+      run do
+        # Run our data requirements operation on the test client server
+        fhir_operation(
+          "Measure/#{INVALID_ID}/$data-requirements?periodEnd=2019-12-31&periodStart=2019-01-01",
+          body: PARAMS
+        )
+        assert_response_status(400)
+        assert_valid_json(response[:body])
+        assert(resource.resourceType == 'OperationOutcome')
+        assert(resource.issue[0].severity == 'error')
       end
     end
     # rubocop:enable Metrics/BlockLength
