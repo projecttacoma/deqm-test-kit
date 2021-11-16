7d5ae607af0fc8e9ea855bca7ff55e790ad53c4b
mriley <mriley@mitre.org>
2021-11-16 10:57:02 -0500

    Fixed issues so inferno will start now

--- lib/deqm_test_kit/bulk_import.rb
+++ lib/deqm_test_kit/bulk_import.rb
@@ -1,24 +1,28 @@
 # frozen_string_literal: true
-
+require_relative '../utils/bulk_import_utils'
 module DEQMTestKit
     # BulkImport test group ensure the fhir server can accept bulk data import requests
     class BulkImport < Inferno::TestGroup
       id 'bulk_import'
       title 'Bulk Import'
       description 'Ensure the fhir server can accept bulk data import requests'
+
       input :measure_id
+      custom_headers = { 'X-Provenance': '{"resourceType": "Provenance"}' , 'prefer': 'respond-async'}
       fhir_client do
         url :url
+        headers custom_headers
       end  
+     
       test do
+
         title 'Ensure data can be accepted'
         id 'bulk-import-01'
         description 'Send the data to the server and the response is a 202 (or is it?)'
-       #need to modify docker compose to start bulk reference server
   
        run do
-
         # get measure from client
+        puts 'made it into the bulk import test group'
         assert(measure_id,
         'No measure selected. Run Measure Availability prior to running the bulk import test group.')
         fhir_read(:measure, measure_id)
@@ -26,7 +30,6 @@ module DEQMTestKit
         measure = resource
         measure_report = create_measure_report(measure.url, '2019-01-01', '2019-12-31')
         params = {
-          {
             "resourceType": "Parameters",
             "parameter": [
               {
@@ -42,46 +45,34 @@ module DEQMTestKit
               }
             ]
           }
-          }
-       fhir_operation("Measure/#{measure_id}/$submit-data", headers:"prefer": "respond-async" ,body: params, name: :submit_data)
-       reply = fhir_client(:url).send(:get, '$bulkstatus')
-       content_location = reply.headers('Content-Location')
-       polling_url = url + reply.headers('Content-Location') #base url plus content location header value
+        fhir_operation("Measure/#{measure_id}/$submit-data", body: params, name: :submit_data)
+        reply = fhir_client(:url).send(:get, '$bulkstatus')
+        content_location = reply.headers('Content-Location')
+        polling_url = url + reply.headers('Content-Location') 
 
-       fhir_operation('$bulkstatus',polling_url)
-       wait_time = 1
-       reply = nil
-       start = Time.now
-      seconds_used = 0
+        fhir_operation('$bulkstatus',polling_url)
+        wait_time = 1
+        reply = nil
+        start = Time.now
+        seconds_used = 0
            
          loop do
               reply = nil
-              begin
-                reply = fhir_client.client.get(polling_url)
-              rescue RestClient::TooManyRequests => e
+               begin
+                 reply = fhir_client.client.get(polling_url)
+               rescue RestClient::TooManyRequests => e
                 reply = e.response
-              end
-              wait_time = get_retry_or_backoff_time(wait_time, reply)
-              seconds_used = Time.now - start
-              # exit loop if we get a successful response or timeout reached
+               end
+               wait_time = get_retry_or_backoff_time(wait_time, reply)
+               seconds_used = Time.now - start
+               # exit loop if we get a successful response or timeout reached
               break if (reply.code != 202 && reply.code != 429) || (seconds_used > timeout)
       
               sleep wait_time
-        end
-      end
-      def self.get_retry_or_backoff_time(wait_time, reply)
-        retry_after = -1
-        unless reply.headers.nil?
-          reply.headers.symbolize_keys
-          retry_after = reply.headers[:retry_after].to_i || -1
-        end
-  
-        if retry_after.positive?
-          retry_after
-        else
-          wait_time * 2
-        end
-      end
+         end
+       end
+      end 
+     
     end
   end
   
---
+++ lib/utils/bulk_import_utils.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module DEQMTestKit
+    module BulkImportUtils
+    # Utility functions in support of the bulk import test group
+
+      def get_retry_or_backoff_time(wait_time, reply)
+        retry_after = -1
+         unless reply.headers.nil?
+           reply.headers.symbolize_keys
+           retry_after = reply.headers[:retry_after].to_i || -1
+         end
+  
+         if retry_after.positive?
+           retry_after
+         else
+           wait_time * 2
+         end
+       end
+    end
+end
