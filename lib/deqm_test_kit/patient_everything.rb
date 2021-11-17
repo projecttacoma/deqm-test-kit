74b402ff9bf118c5a398088648516c08b2e3fedf
sarahmcdougall <smcdougall@mitre.org>
2021-11-17 11:54:33 -0500

    cleaned up comments, vars

--- lib/deqm_test_kit/patient_everything.rb
+++ lib/deqm_test_kit/patient_everything.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 require "json"
 
-
-
-
 module DEQMTestKit
   # Perform Patient/$everything operation on test client
   class PatientEverything < Inferno::TestGroup
@@ -15,29 +12,29 @@ module DEQMTestKit
       url :url
     end
 
-    TEST_PATIENT_ID = 'test-patient'
+    TEST_PATIENT_ID = 'test-patient' 
 
     # rubocop:disable Metrics/BlockLength
     test do
       title 'Patient/<id>/$everything valid submission'
       id 'patient-everything-01'
-      description 'Patient data is received for patient on the server'
+      description 'Patient data is received for single patient on the server'
       makes_request :patient_everything
       output :queries_json
 
       run do
-        singlePatientBundle = File.open('./lib/fixtures/singlePatientBundle.json')
-        singlePatientData = JSON.parse(singlePatientBundle.read)
+        singlePatientFile = File.open('./lib/fixtures/singlePatientBundle.json')
+        singlePatientBundle = JSON.parse(singlePatientFile.read)
         # Upload single patient bundle to server
-        fhir_operation("/", body: singlePatientData)
-        # Run our Patient/<id>/$everything operation on the test client server
+        fhir_operation("/", body: singlePatientBundle)
+        # Run Patient/<id>/$everything operation on the test client server
         fhir_operation("Patient/#{TEST_PATIENT_ID}/$everything", name: :patient_everything)
         assert_response_status(200)
         assert_resource_type(:bundle)
         assert_valid_json(response[:body])
         # Check all necessary resources are included in the response
-        # Note Condition resource does not include patient ref
-        assert(singlePatientData["entry"].length() - 1 == resource.total, "Expected #{singlePatientData["entry"].length() - 1} in response but received #{resource.total}")
+        # Note Condition resource does not include patient ref, so subtract 1
+        assert(singlePatientBundle["entry"].length() - 1 == resource.total, "Expected #{singlePatientBundle["entry"].length() - 1} in response but received #{resource.total}")
         # Check that all necessary resources are included in response
         assert(resource.entry.count { |x| x.resource.resourceType == "Encounter"} == 1)
         assert(resource.entry.count { |x| x.resource.resourceType == "Procedure"} == 1)
@@ -53,25 +50,24 @@ module DEQMTestKit
       output :queries_json
 
       run do
-        multiplePatientBundle = File.open('./lib/fixtures/multiplePatientBundle.json')
-        multiplePatientData = JSON.parse(multiplePatientBundle.read)
+        multiplePatientFile = File.open('./lib/fixtures/multiplePatientBundle.json')
+        multiplePatientBundle = JSON.parse(multiplePatientFile.read)
         # Upload multiple patient bundle to server
-        fhir_operation("/", body: multiplePatientData)
-        # Run our Patient/$everything operation on the test client server
+        fhir_operation("/", body: multiplePatientBundle)
+        # Run Patient/$everything operation on the test client server
         fhir_operation("Patient/$everything", name: :patient_everything)
         assert_response_status(200)
         assert_resource_type(:bundle)
         assert_valid_json(response[:body])
         # Check all necessary resources are included in the response
         # Note all resources should be included because they all reference a patient
-        # Use >=, as other data may be retrieved in addition to what we expect
-        assert(resource.total >= multiplePatientData["entry"].length())
+        # Use >=, as other data may be retrieved from server in addition to what we expect
+        assert(resource.total >= multiplePatientBundle["entry"].length())
         # Check that all necessary resources are included in response
         assert(resource.entry.count { |x| x.resource.resourceType == "Patient"} >= 2)
         assert(resource.entry.count { |x| x.resource.resourceType == "Encounter"} >= 1)
         assert(resource.entry.count { |x| x.resource.resourceType == "Procedure"} >= 1)
         assert(resource.entry.count { |x| x.resource.resourceType == "Condition"} >= 1)
-
       end
     end
 
@@ -82,7 +78,7 @@ module DEQMTestKit
       makes_request :patient_everything
 
       run do
-        # Run our Patient/$everything operation on the test client server with invalid id
+        # Run Patient/$everything operation on the test client server with invalid id
         fhir_operation("Patient/INVALID/$everything", name: :patient_everything)
         assert_valid_json(response[:body])
         assert_response_status(404)
--- spec/deqm_test_kit/patient_everything_spec.rb
+++ spec/deqm_test_kit/patient_everything_spec.rb
@@ -1,28 +1,30 @@
 # frozen_string_literal: true
 require "json"
 
-$singlePatientBundle = File.open('./lib/fixtures/singlePatientBundle.json')
-$singlePatientData = JSON.parse($singlePatientBundle.read).to_json
-$multiplePatientBundle = File.open('./lib/fixtures/multiplePatientBundle.json')
-$multiplePatientData = JSON.parse($multiplePatientBundle.read).to_json
-
+# single patient example for Patient/<id>/$everything
+$singlePatientFile = File.open('./lib/fixtures/singlePatientBundle.json')
+$singlePatientBundle = JSON.parse($singlePatientFile.read).to_json
 $singlePatientResponseFile = File.open('./spec/fixtures/singlePatientResponse.json')
 $singlePatientResponse = JSON.parse($singlePatientResponseFile.read).to_json
-$multiplePatientResponseFile = File.open('./spec/fixtures/multiplePatientResponse.json')
-$multiplePatientResponse = JSON.parse($multiplePatientResponseFile.read).to_json
 
-$singlePatientLength = $singlePatientData.length()
-$multiplePatientLength = $multiplePatientData.length()
-
+$singlePatientLength = $singlePatientBundle.length()
 $singleHeaders = {'Accept'=>'application/fhir+json',
 'Accept-Charset'=>'utf-8',
 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-'Content-Length'=>$singlePatientLength.to_s,
+'Content-Length'=> $singlePatientLength.to_s,
 'Content-Type'=>'application/fhir+json',
 'Host'=>'example.com',
 'User-Agent'=>'Ruby FHIR Client'
 }
 
+# multiple patient example for Patient/$everything
+$multiplePatientFile = File.open('./lib/fixtures/multiplePatientBundle.json')
+$multiplePatientBundle = JSON.parse($multiplePatientFile.read).to_json
+$multiplePatientResponseFile = File.open('./spec/fixtures/multiplePatientResponse.json')
+$multiplePatientResponse = JSON.parse($multiplePatientResponseFile.read).to_json
+
+
+$multiplePatientLength = $multiplePatientBundle.length()
 $multHeaders = {'Accept'=>'application/fhir+json',
 'Accept-Charset'=>'utf-8',
 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
@@ -53,14 +55,14 @@ RSpec.describe DEQMTestKit::PatientEverything do
         let(:test) { group.tests.first }
 
         it 'passes for valid request to Patient/<id>/$everything' do
-            stub_request(:post, "#{url}/").with(body: $singlePatientData, headers: $singleHeaders).to_return(status: 200, body: "", headers: {})
+            stub_request(:post, "#{url}/").with(body: $singlePatientBundle, headers: $singleHeaders).to_return(status: 200, body: "", headers: {})
             stub_request(:post, "#{url}/Patient/test-patient/$everything").to_return(status: 200, body: $singlePatientResponse, headers: {})
             result = run(test, url: url)
             expect(result.result).to eq('pass')
         end
 
         it 'fails when server does not return 200' do
-            stub_request(:post, "#{url}/").with(body: $singlePatientData, headers: $singleHeaders).to_return(status: 200, body: "", headers: {})
+            stub_request(:post, "#{url}/").with(body: $singlePatientBundle, headers: $singleHeaders).to_return(status: 200, body: "", headers: {})
             stub_request(:post, "#{url}/Patient/test-patient/$everything").to_return(status: 404, body: error_outcome.to_json)
             result = run(test, url: url)
             expect(result.result).to eq('fail')
@@ -72,14 +74,14 @@ RSpec.describe DEQMTestKit::PatientEverything do
         let(:test) { group.tests[1] }
 
         it 'passes for valid request to Patient/$everything' do
-            stub_request(:post, "#{url}/").with(body: $multiplePatientData, headers: $multHeaders).to_return(status: 200, body: "", headers: {})
+            stub_request(:post, "#{url}/").with(body: $multiplePatientBundle, headers: $multHeaders).to_return(status: 200, body: "", headers: {})
             stub_request(:post, "#{url}/Patient/$everything").to_return(status: 200, body: $multiplePatientResponse, headers: {})
             result = run(test, url: url)
             expect(result.result).to eq('pass')
         end
 
         it 'fails when server does not return 200' do
-            stub_request(:post, "#{url}/").with(body: $multiplePatientData, headers: $multHeaders).to_return(status: 200, body: "", headers: {})
+            stub_request(:post, "#{url}/").with(body: $multiplePatientBundle, headers: $multHeaders).to_return(status: 200, body: "", headers: {})
             stub_request(:post, "#{url}/Patient/$everything").to_return(status: 404, body: error_outcome.to_json)
             result = run(test, url: url)
             expect(result.result).to eq('fail')
