baf31e40b888731e6172db1243b5fd874f9365ea
mriley <mriley@mitre.org>
2021-11-17 09:50:08 -0500

    Addressed linter issues

--- lib/deqm_test_kit/bulk_import.rb
+++ lib/deqm_test_kit/bulk_import.rb
@@ -1,83 +1,72 @@
 # frozen_string_literal: true
+
 require_relative '../utils/bulk_import_utils'
 module DEQMTestKit
-    # BulkImport test group ensure the fhir server can accept bulk data import requests
-    class BulkImport < Inferno::TestGroup
-      id 'bulk_import'
-      title 'Bulk Import'
-      description 'Ensure the fhir server can accept bulk data import requests'
+  # BulkImport test group ensure the fhir server can accept bulk data import requests
+  class BulkImport < Inferno::TestGroup
+    id 'bulk_import'
+    title 'Bulk Import'
+    description 'Ensure the fhir server can accept bulk data import requests'
 
-      input :measure_id
-      custom_headers = { 'X-Provenance': '{"resourceType": "Provenance"}' , 'prefer': 'respond-async'}
-      fhir_client do
-        url :url
-        headers custom_headers
-      end  
-     
-      test do
+    input :measure_id
+    custom_headers = { 'X-Provenance': '{"resourceType": "Provenance"}', prefer: 'respond-async' }
+    fhir_client do
+      url :url
+      headers custom_headers
+    end
+    test do
+      title 'Ensure data can be accepted'
+      id 'bulk-import-01'
+      description 'Send the data to the server and the response is a 202 (or is it?)'
 
-        title 'Ensure data can be accepted'
-        id 'bulk-import-01'
-        description 'Send the data to the server and the response is a 202 (or is it?)'
-  
-       run do
+      run do
         # get measure from client
         puts 'made it into the bulk import test group'
         assert(measure_id,
-        'No measure selected. Run Measure Availability prior to running the bulk import test group.')
+               'No measure selected. Run Measure Availability prior to running the bulk import test group.')
         fhir_read(:measure, measure_id)
         assert_valid_json(response[:body])
         measure = resource
         measure_report = create_measure_report(measure.url, '2019-01-01', '2019-12-31')
         params = {
-            "resourceType": "Parameters",
-            "parameter": [
-              {
-                "name": "measureReport",
-                "resource": {
-                  "resourceType": "MeasureReport",
-                  "measure": "http://hl7.org/fhir/us/cqfmeasures/Measure/EXM130"
-                }
-              },
-              {
-                "name": "exportURL",
-                "valueString": "https://bulk-data.smarthealthit.org/eyJlcnIiOiIiLCJwYWdlIjoxMDAwMCwiZHVyIjoxMCwidGx0IjoxNSwibSI6MSwic3R1IjozLCJkZWwiOjB9/fhir"
+          resourceType: 'Parameters',
+          parameter: [
+            {
+              name: 'measureReport',
+              resource: {
+                resourceType: 'MeasureReport',
+                measure: 'http://hl7.org/fhir/us/cqfmeasures/Measure/EXM130'
               }
-            ]
-          }
+            },
+            {
+              name: 'exportURL',
+              valueString: 'https://bulk-data.smarthealthit.org/eyJlcnIiOiIiLCJwYWdlIjoxMDAwMCwiZHVyIjoxMCwidGx0IjoxNSwibSI6MSwic3R1IjozLCJkZWwiOjB9/fhir'
+            }
+          ]
+        }
         fhir_operation("Measure/#{measure_id}/$submit-data", body: params, name: :submit_data)
         reply = fhir_client(:url).send(:get, '$bulkstatus')
-        content_location = reply.headers('Content-Location')
-        polling_url = url + reply.headers('Content-Location') 
-
-        fhir_operation('$bulkstatus',polling_url)
+        polling_url = url + reply.headers('Content-Location')
+        fhir_operation('$bulkstatus', polling_url)
         wait_time = 1
         reply = nil
         start = Time.now
         seconds_used = 0
-           
-         loop do
-              reply = nil
-               begin
-                 reply = fhir_client.client.get(polling_url)
-               rescue RestClient::TooManyRequests => e
-                reply = e.response
-               end
-               wait_time = get_retry_or_backoff_time(wait_time, reply)
-               seconds_used = Time.now - start
-               # exit loop if we get a successful response or timeout reached
-              break if (reply.code != 202 && reply.code != 429) || (seconds_used > timeout)
-      
-              sleep wait_time
-         end
-       end
-      end 
-     
+        loop do
+          reply = nil
+          begin
+            reply = fhir_client.client.get(polling_url)
+          rescue RestClient::TooManyRequests => e
+            reply = e.response
+          end
+          wait_time = get_retry_or_backoff_time(wait_time, reply)
+          seconds_used = Time.now - start
+          # exit loop if we get a successful response or timeout reached
+          break if (reply.code != 202 && reply.code != 429) || (seconds_used > timeout)
+
+          sleep wait_time
+        end
+      end
     end
   end
-  
-
-  #remember that the bulk import request is a measure report and the link to the bulk export server 
-  #(the test server can handle all the other logic)
-  # the export server can be the link to the smart on fhir  
-  #don't need to loop or create every resource can just hardcode link to bulk export server
+end
--- lib/utils/bulk_import_utils.rb
+++ lib/utils/bulk_import_utils.rb
@@ -1,21 +1,18 @@
 # frozen_string_literal: true
-
+# Utility functions in support of the bulk import test group
 module DEQMTestKit
-    module BulkImportUtils
-    # Utility functions in support of the bulk import test group
-
+ module BulkImportUtils
       def get_retry_or_backoff_time(wait_time, reply)
         retry_after = -1
-         unless reply.headers.nil?
+        unless reply.headers.nil?
            reply.headers.symbolize_keys
            retry_after = reply.headers[:retry_after].to_i || -1
          end
-  
          if retry_after.positive?
            retry_after
          else
            wait_time * 2
          end
-       end
-    end
+      end
+  end
 end
