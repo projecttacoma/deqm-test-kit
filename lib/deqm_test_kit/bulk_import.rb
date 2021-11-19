9852885c63890f82ec0976d108d0bd5de6a66fda
lmd59 <laurend@mitre.org>
2021-11-19 16:21:28 -0500

    some updates to make headers work and debug more easily

--- lib/deqm_test_kit/bulk_import.rb
+++ lib/deqm_test_kit/bulk_import.rb
@@ -36,31 +36,41 @@ module DEQMTestKit
       id 'bulk-import-01'
       description 'Send the data to the server and the response is a 202 (or is it?)'
       run do
-        assert(measure_id,
-               'No measure selected. Run Measure Availability prior to running the bulk import test group.')
-        fhir_read(:measure, measure_id)
-        assert_valid_json(response[:body])
-        fhir_operation("Measure/#{measure_id}/$submit-data", body: params, name: :submit_data)
-        reply = fhir_client(:url).send(:get, '$bulkstatus')
-        polling_url = url + reply.headers('Content-Location')
-        fhir_operation('$bulkstatus', polling_url)
-        wait_time = 1
-        reply = nil
-        start = Time.now
-        seconds_used = 0
-        loop do
-          reply = nil
-          begin
-            reply = fhir_client.client.get(polling_url)
-          rescue RestClient::TooManyRequests => e
-            reply = e.response
+        begin
+          assert(measure_id,
+                'No measure selected. Run Measure Availability prior to running the bulk import test group.')
+          fhir_read(:measure, measure_id)
+          assert_valid_json(response[:body])
+          fhir_operation("Measure/#{measure_id}/$submit-data", body: params, name: :submit_data)
+          reply = fhir_client.send(:get, '$bulkstatus')
+          location_header = response[:headers].find { |h| h.name == 'content-location'}
+          # temporary fix for extra 4_0_1
+          polling_url = "#{url}/#{location_header.value.sub("4_0_1/", '')}"
+          # fhir_operation('$bulkstatus', body: polling_url)
+          fhir_client do
+            url polling_url
           end
-          wait_time = get_retry_or_backoff_time(wait_time, reply)
-          seconds_used = Time.now - start
-          # exit loop if we get a successful response or timeout reached
-          break if (reply.code != 202 && reply.code != 429) || (seconds_used > timeout)
+          reply = fhir_client.send(:get, "")
+          wait_time = 1
+          reply = nil
+          start = Time.now
+          seconds_used = 0
+          loop do
+            reply = nil
+            begin
+              reply = fhir_client.client.get(polling_url)
+            rescue RestClient::TooManyRequests => e
+              reply = e.response
+            end
+            wait_time = get_retry_or_backoff_time(wait_time, reply)
+            seconds_used = Time.now - start
+            # exit loop if we get a successful response or timeout reached
+            break if (reply.code != 202 && reply.code != 429) || (seconds_used > timeout)
 
-          sleep wait_time
+            sleep wait_time
+          end
+        rescue => e
+          assert false, ("url: #{polling_url}, error: #{e.message}, backtrace: #{e.backtrace}")
         end
       end
     end
