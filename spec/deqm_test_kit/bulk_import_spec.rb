110c9a9a90fafbd550b2d579f708d13e9a6072b4
mriley <mriley@mitre.org>
2021-11-19 11:51:31 -0500

    Create bulk_import_spec.rb

---
+++ spec/deqm_test_kit/bulk_import_spec.rb
@@ -0,0 +1,28 @@
+RSpec.describe DEQMTestKit::BulkImport do
+    let(:suite) { Inferno::Repositories::TestSuites.new.find('deqm_test_suite') }
+    let(:group) { suite.groups[2] }
+    let(:session_data_repo) { Inferno::Repositories::SessionData.new }
+    let(:test_session) { repo_create(:test_session, test_suite_id: 'deqm_test_suite') }
+    url = 'http://example.com/fhir'
+    # ensure this url matches url in embedded_client in data_requirements.rb
+    let(:embedded_client) do
+      'http://cqf_ruler:8080/cqf-ruler-r4/fhir'
+    end
+    let(:error_outcome) { FHIR::OperationOutcome.new(issue: [{ severity: 'error' }]) }
+    let(:test_library_response) { FHIR::Library.new(dataRequirement: [{ type: 'Condition' }]) }
+    let(:incorrect_severity) { FHIR::OperationOutcome.new(issue: [{ severity: 'warning' }]) }
+    def run(runnable, inputs = {})
+        test_run_params = { test_session_id: test_session.id }.merge(runnable.reference_hash)
+        test_run = Inferno::Repositories::TestRuns.new.create(test_run_params)
+        inputs.each do |name, value|
+          session_data_repo.save(test_session_id: test_session.id, name: name, value: value)
+        end
+        Inferno::TestRunner.new(test_session: test_session, test_run: test_run).run(runnable)
+    end
+    describe 'data requirements matches embedded results test' do
+      let(:test) { group.tests.first }
+      let(:measure_name) { 'EXM130' }
+      let(:measure_version) { '7.3.000' }
+      let(:measure_id) { 'measure-EXM130-7.3.000' }
+    end
+end
