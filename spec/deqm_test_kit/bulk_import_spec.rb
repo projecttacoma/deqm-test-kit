630a5c294de6455be59e3e37ce443a72e63b7121
mriley <mriley@mitre.org>
2021-11-29 15:30:00 -0500

    Update bulk_import_spec.rb

--- spec/deqm_test_kit/bulk_import_spec.rb
+++ spec/deqm_test_kit/bulk_import_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe DEQMTestKit::BulkImport do
     end
     it 'can proceed since the measure exists' do
       resource = FHIR::Bundle.new(total: 1, entry: [{ resource: { id: 'test_id' } }])
-      stub_request(:get, "#{url}'/bulkstatus'", custom_headers).to_return(status: 202, body: resource.to_json)
+      stub_request(:get, "#{url}'/4_0_1/'").to_return(status: 202, body: resource.to_json)
       result = run(test, url: url)
       # check that we get a 202 off a bulk data request
       expect(result.result).to eq('pass')
