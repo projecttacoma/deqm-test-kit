9feb11ed98161507278e908c0f1ace591cdc90fa
sarahmcdougall <smcdougall@mitre.org>
2021-11-30 11:23:40 -0500

    cleaned up param code for rspec

--- spec/deqm_test_kit/evaluate_measure_spec.rb
+++ spec/deqm_test_kit/evaluate_measure_spec.rb
@@ -23,16 +23,15 @@ RSpec.describe DEQMTestKit::EvaluateMeasure do
     let(:patient_id) { 'numer-EXM130' }
     let(:period_start) { '2019-01-01' }
     let(:period_end) { '2019-12-31' }
+    let(:params) { "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{patient_id}" }
 
     it 'passes if request has valid parameters, patient id, and measure id' do
-      params = "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{patient_id}"
       test_measure_report = FHIR::MeasureReport.new(entry: [{ resource: {resourceType: 'MeasureReport', measure: measure_id}}])
       stub_request(:post, "#{url}/Measure/#{measure_id}/$evaluate-measure?#{params}")
         .to_return(status: 200, body: test_measure_report.to_json)
     end
 
     it 'fails if $evaluate-measure does not return 200' do
-      params = "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{patient_id}" 
       stub_request(:post, "#{url}/Measure/#{measure_id}/$evaluate-measure?#{params}").to_return(status: 404, body: error_outcome.to_json) 
       result = run(test, url: url, measure_id: measure_id, patient_id: patient_id, period_start: period_start, period_end: period_end)
          expect(result.result).to eq('fail')
@@ -45,11 +44,11 @@ RSpec.describe DEQMTestKit::EvaluateMeasure do
     let(:patient_id) { 'numer-EXM130' }
     let(:period_start) { '2019-01-01' }
     let(:period_end) { '2019-12-31' }
+    let(:params) { "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{patient_id}" }
 
     INVALID_MEASURE_ID = 'INVALID_MEASURE_ID'
 
     it 'passes with correct Operation-Outcome returned' do
-      params = "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{patient_id}"
       stub_request(:post, "#{url}/Measure/#{INVALID_MEASURE_ID}/$evaluate-measure?#{params}")
         .to_return(status: 404, body: error_outcome.to_json)
       result = run(test, url: url, patient_id: patient_id, period_start: period_start, period_end: period_end)
@@ -57,7 +56,6 @@ RSpec.describe DEQMTestKit::EvaluateMeasure do
     end
 
     it 'fails if server does not return 404 for invalid measure id' do
-      params = "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{patient_id}" 
       stub_request(:post, "#{url}/Measure/#{INVALID_MEASURE_ID}/$evaluate-measure?#{params}").to_return(status: 200, body: error_outcome.to_json) 
       result = run(test, url: url, patient_id: patient_id, period_start: period_start, period_end: period_end)
          expect(result.result).to eq('fail')
@@ -70,11 +68,11 @@ RSpec.describe DEQMTestKit::EvaluateMeasure do
     let(:measure_id) { 'measure-EXM130-7.3.000' }
     let(:period_start) { '2019-01-01' }
     let(:period_end) { '2019-12-31' }
+    let(:params) { "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{INVALID_PATIENT_ID}" }
 
     INVALID_PATIENT_ID = 'INVALID_PATIENT_ID'
 
     it 'passes with correct Operation-Outcome returned' do
-      params = "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{INVALID_PATIENT_ID}"
       stub_request(:post, "#{url}/Measure/#{measure_id}/$evaluate-measure?#{params}")
         .to_return(status: 404, body: error_outcome.to_json)
       result = run(test, url: url, measure_id: measure_id, period_start: period_start, period_end: period_end)
@@ -82,7 +80,6 @@ RSpec.describe DEQMTestKit::EvaluateMeasure do
     end
 
     it 'fails if server does not return 404 for invalid patient id' do
-      params = "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{INVALID_PATIENT_ID}" 
       stub_request(:post, "#{url}/Measure/#{measure_id}/$evaluate-measure?#{params}").to_return(status: 200, body: error_outcome.to_json) 
       result = run(test, url: url, measure_id: measure_id, period_start: period_start, period_end: period_end)
          expect(result.result).to eq('fail')
@@ -95,9 +92,9 @@ RSpec.describe DEQMTestKit::EvaluateMeasure do
     let(:measure_id) { 'measure-EXM130-7.3.000' }
     let(:patient_id) { 'numer-EXM130' }
     let(:period_end) { '2019-12-31' }
+    let(:params) { "periodEnd=#{period_end}&subject=#{patient_id}" }
 
     it 'passes with correct Operation-Outcome returned' do
-      params = "periodEnd=#{period_end}&subject=#{patient_id}"
       stub_request(:post, "#{url}/Measure/#{measure_id}/$evaluate-measure?#{params}")
         .to_return(status: 400, body: error_outcome.to_json)
       result = run(test, url: url, measure_id: measure_id, patient_id: patient_id, period_end: period_end)
@@ -105,7 +102,6 @@ RSpec.describe DEQMTestKit::EvaluateMeasure do
     end
 
     it 'fails if server does not return 400 for missing param' do
-      params = "periodEnd=#{period_end}&subject=#{patient_id}" 
       stub_request(:post, "#{url}/Measure/#{measure_id}/$evaluate-measure?#{params}").to_return(status: 200, body: error_outcome.to_json) 
       result = run(test, url: url, measure_id: measure_id, patient_id: patient_id, period_end: period_end)
          expect(result.result).to eq('fail')
@@ -118,9 +114,9 @@ RSpec.describe DEQMTestKit::EvaluateMeasure do
     let(:measure_id) { 'measure-EXM130-7.3.000' }
     let(:period_start) { '2019-01-01' }
     let(:period_end) { '2019-12-31' }
+    let(:params) { "periodStart=#{period_start}&periodEnd=#{period_end}" }
 
     it 'passes with correct Operation-Outcome returned' do
-      params = "periodStart=#{period_start}&periodEnd=#{period_end}"
       stub_request(:post, "#{url}/Measure/#{measure_id}/$evaluate-measure?#{params}")
         .to_return(status: 400, body: error_outcome.to_json)
       result = run(test, url: url, measure_id: measure_id, period_start: period_start, period_end: period_end)
@@ -128,7 +124,6 @@ RSpec.describe DEQMTestKit::EvaluateMeasure do
     end
 
     it 'fails if server does not return 400 for missing subject param' do
-      params = "periodStart=#{period_start}&periodEnd=#{period_end}" 
       stub_request(:post, "#{url}/Measure/#{measure_id}/$evaluate-measure?#{params}").to_return(status: 200, body: error_outcome.to_json) 
       result = run(test, url: url, measure_id: measure_id, period_start: period_start, period_end: period_end)
          expect(result.result).to eq('fail')
@@ -142,9 +137,9 @@ RSpec.describe DEQMTestKit::EvaluateMeasure do
     let(:patient_id) { 'numer-EXM130' }
     let(:period_start) { '2019-01-01' }
     let(:period_end) { '2019-12-31' }
+    let(:params) { "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{patient_id}&reportType=subject-list" }
 
     it 'passes with correct Operation-Outcome returned' do
-      params = "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{patient_id}&reportType=subject-list"
       stub_request(:post, "#{url}/Measure/#{measure_id}/$evaluate-measure?#{params}")
         .to_return(status: 501, body: error_outcome.to_json)
       result = run(test, url: url, measure_id: measure_id, patient_id: patient_id, period_start: period_start, period_end: period_end)
@@ -152,7 +147,6 @@ RSpec.describe DEQMTestKit::EvaluateMeasure do
     end
 
     it 'fails if server does not return 501 for unsupported subject-list' do
-      params = "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{patient_id}&reportType=subject-list" 
       stub_request(:post, "#{url}/Measure/#{measure_id}/$evaluate-measure?#{params}").to_return(status: 200, body: error_outcome.to_json) 
       result = run(test, url: url, measure_id: measure_id, patient_id: patient_id, period_start: period_start, period_end: period_end)
          expect(result.result).to eq('fail')
@@ -166,9 +160,9 @@ RSpec.describe DEQMTestKit::EvaluateMeasure do
     let(:patient_id) { 'numer-EXM130' }
     let(:period_start) { '2019-01-01' }
     let(:period_end) { '2019-12-31' }
+    let(:params) { "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{patient_id}&lastReceivedOn=2019-12-31" }
 
     it 'passes with correct Operation-Outcome returned' do
-      params = "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{patient_id}&lastReceivedOn=2019-12-31"
       stub_request(:post, "#{url}/Measure/#{measure_id}/$evaluate-measure?#{params}")
         .to_return(status: 400, body: error_outcome.to_json)
       result = run(test, url: url, measure_id: measure_id, patient_id: patient_id, period_start: period_start, period_end: period_end)
@@ -176,7 +170,6 @@ RSpec.describe DEQMTestKit::EvaluateMeasure do
     end
 
     it 'fails if server does not return 400 for unsupported param' do
-      params = "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{patient_id}&lastReceivedOn=2019-12-31" 
       stub_request(:post, "#{url}/Measure/#{measure_id}/$evaluate-measure?#{params}").to_return(status: 200, body: error_outcome.to_json) 
       result = run(test, url: url, measure_id: measure_id, patient_id: patient_id, period_start: period_start, period_end: period_end)
          expect(result.result).to eq('fail')
@@ -190,9 +183,9 @@ RSpec.describe DEQMTestKit::EvaluateMeasure do
     let(:patient_id) { 'numer-EXM130' }
     let(:period_start) { '2019-01-01' }
     let(:period_end) { '2019-12-31' }
+    let(:params) { "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{patient_id}&reportType=INVALID" }
 
     it 'passes with correct Operation-Outcome returned' do
-      params = "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{patient_id}&reportType=INVALID"
       stub_request(:post, "#{url}/Measure/#{measure_id}/$evaluate-measure?#{params}")
         .to_return(status: 400, body: error_outcome.to_json)
       result = run(test, url: url, measure_id: measure_id, patient_id: patient_id, period_start: period_start, period_end: period_end)
@@ -200,7 +193,6 @@ RSpec.describe DEQMTestKit::EvaluateMeasure do
     end
 
     it 'fails if server does not return 400 for invalid reportType' do
-      params = "periodStart=#{period_start}&periodEnd=#{period_end}&subject=#{patient_id}&reportType=INVALID" 
       stub_request(:post, "#{url}/Measure/#{measure_id}/$evaluate-measure?#{params}").to_return(status: 200, body: error_outcome.to_json) 
       result = run(test, url: url, measure_id: measure_id, patient_id: patient_id, period_start: period_start, period_end: period_end)
          expect(result.result).to eq('fail')
