8916bb8933aa38d99735b118f1afb19e444a19ed
natjoe4 <njjones@mitre.org>
2021-12-07 10:44:47 -0500

    updated suite index

--- spec/deqm_test_kit/care_gaps_spec.rb
+++ spec/deqm_test_kit/care_gaps_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe DEQMTestKit::DataRequirements do
   let(:suite) { Inferno::Repositories::TestSuites.new.find('deqm_test_suite') }
-  let(:group) { suite.groups[6] }
+  let(:group) { suite.groups[5] }
   let(:session_data_repo) { Inferno::Repositories::SessionData.new }
   let(:test_session) { repo_create(:test_session, test_suite_id: 'deqm_test_suite') }
   url = 'http://example.com/fhir'
