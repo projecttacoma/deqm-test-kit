7e89e80f0da87a4e2a737cc39e86035b028bdac2
lmd59 <laurend@mitre.org>
2022-03-29 11:52:30 -0400

    Add tests for program, practitioner, organization to the care gaps tests

--- spec/deqm_test_kit/care_gaps_spec.rb
+++ spec/deqm_test_kit/care_gaps_spec.rb
@@ -283,4 +283,88 @@ RSpec.describe DEQMTestKit::CareGaps do
       expect(result.result).to eq('fail')
     end
   end
+  describe '$care-gaps successful test with practitioner and organization' do
+    let(:test) { group.tests[7] }
+    let(:measure_id) { 'measure-EXM130-7.3.000' }
+    let(:practitioner_id) { '1' }
+    let(:org_id) { '1' }
+    let(:period_start) { '2019-01-01' }
+    let(:period_end) { '2019-12-31' }
+    let(:test_parameters) { FHIR::Parameters.new(total: 1) }
+    let(:error_outcome) { FHIR::OperationOutcome.new(issue: [{ severity: 'error' }]) }
+
+    let(:params) do
+      "measureId=#{measure_id}&periodStart=#{period_start}&periodEnd=#{period_end}"\
+        "&practitioner=Practitioner/#{practitioner_id}&organization=Organization/#{org_id}&status=open-gap"
+    end
+    test_parameters = FHIR::Parameters.new(total: 1)
+    it 'passes if request has valid parameters' do
+      stub_request(
+        :post,
+        "#{url}/Measure/$care-gaps?#{params}"
+      ).to_return(status: 200, body: test_parameters.to_json)
+      result = run(test, url: url, measure_id: measure_id, practitioner_id: practitioner_id, org_id: org_id,
+                         period_start: period_start, period_end: period_end)
+      expect(result.result).to eq('pass')
+    end
+    it 'fails if $care-gaps does not return 200' do
+      stub_request(
+        :post,
+        "#{url}/Measure/$care-gaps?#{params}"
+      ).to_return(status: 400, body: test_parameters.to_json)
+      result = run(test, url: url, measure_id: measure_id, practitioner_id: practitioner_id, org_id: org_id,
+                         period_start: period_start, period_end: period_end)
+      expect(result.result).to eq('fail')
+    end
+    it 'fails if $care-gaps does not return Parameters object' do
+      stub_request(
+        :post,
+        "#{url}/Measure/$care-gaps?#{params}"
+      ).to_return(status: 200, body: error_outcome.to_json)
+      result = run(test, url: url, measure_id: measure_id, practitioner_id: practitioner_id, org_id: org_id,
+                         period_start: period_start, period_end: period_end)
+      expect(result.result).to eq('fail')
+    end
+  end
+  describe '$care-gaps successful test with program' do
+    let(:test) { group.tests[8] }
+    let(:patient_id) { 'numer-EXM130' }
+    let(:period_start) { '2019-01-01' }
+    let(:period_end) { '2019-12-31' }
+    let(:test_parameters) { FHIR::Parameters.new(total: 1) }
+    let(:error_outcome) { FHIR::OperationOutcome.new(issue: [{ severity: 'error' }]) }
+
+    let(:params) do
+      "program=eligible-provider&periodStart=#{period_start}&periodEnd=#{period_end}"\
+        "&subject=Patient/#{patient_id}&status=open-gap"
+    end
+    test_parameters = FHIR::Parameters.new(total: 1)
+    it 'passes if request has valid parameters' do
+      stub_request(
+        :post,
+        "#{url}/Measure/$care-gaps?#{params}"
+      ).to_return(status: 200, body: test_parameters.to_json)
+      result = run(test, url: url, program: 'eligible-provider', patient_id: patient_id, period_start: period_start,
+                         period_end: period_end)
+      expect(result.result).to eq('pass')
+    end
+    it 'fails if $care-gaps does not return 200' do
+      stub_request(
+        :post,
+        "#{url}/Measure/$care-gaps?#{params}"
+      ).to_return(status: 400, body: test_parameters.to_json)
+      result = run(test, url: url, program: 'eligible-provider', patient_id: patient_id, period_start: period_start,
+                         period_end: period_end)
+      expect(result.result).to eq('fail')
+    end
+    it 'fails if $care-gaps does not return Parameters object' do
+      stub_request(
+        :post,
+        "#{url}/Measure/$care-gaps?#{params}"
+      ).to_return(status: 200, body: error_outcome.to_json)
+      result = run(test, url: url, program: 'eligible-provider', patient_id: patient_id, period_start: period_start,
+                         period_end: period_end)
+      expect(result.result).to eq('fail')
+    end
+  end
 end
