a0553e2f1450ee9fc1318ae04c4ed94fc61cf3a3
lmd59 <laurend@mitre.org>
2023-04-28 10:29:38 -0400

    Update measure availability default

--- lib/deqm_test_kit/measure_availability.rb
+++ lib/deqm_test_kit/measure_availability.rb
@@ -22,7 +22,7 @@ module DEQMTestKit
     end
 
     measure_options = JSON.parse(File.read('./lib/fixtures/measureAvailabilityRadioButton.json'))
-    measure_id_args = { type: 'radio', optional: false, default: 'EXM130|7.3.000', options: measure_options }
+    measure_id_args = { type: 'radio', optional: false, default: 'ColorectalCancerScreeningsFHIR|0.0.003', options: measure_options }
 
     test do
       include MeasureAvailabilityHelpers
