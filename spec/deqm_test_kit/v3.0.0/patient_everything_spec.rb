441ebbceb2563171e3383e77cd6cf1d33e4674d0
Elsa <eperelli@mitre.org>
2026-04-22 11:45:23 -0400

    rspec test fixes

--- spec/deqm_test_kit/v3.0.0/patient_everything_spec.rb
+++ spec/deqm_test_kit/v3.0.0/patient_everything_spec.rb
@@ -23,20 +23,12 @@ RSpec.describe DEQMTestKit::PatientEverything do
     let(:test) { test_by_id(group, 'patient-everything-single-patient') }
 
     # single patient example for Patient/<id>/$everything
-    single_patient_file = File.open('./lib/fixtures/singlePatientBundle.json')
-    single_patient_bundle = JSON.parse(single_patient_file.read).to_json
-    single_patient_response_file = File.open('./spec/fixtures/singlePatientResponse.json')
-    single_patient_response = JSON.parse(single_patient_response_file.read).to_json
+    single_patient_bundle = JSON.parse(File.read('./lib/fixtures/singlePatientBundle.json'))
+    single_patient_response = File.read('./spec/fixtures/singlePatientResponse.json')
 
-    single_patient_length = single_patient_bundle.length
     single_headers = {
       'Accept' => 'application/fhir+json',
-      'Accept-Charset' => 'utf-8',
-      'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      'Content-Length' => single_patient_length.to_s,
-      'Content-Type' => 'application/fhir+json',
-      'Host' => 'example.com',
-      'User-Agent' => 'Ruby FHIR Client'
+      'Content-Type' => 'application/fhir+json'
     }
 
     it 'passes for valid request to Patient/<id>/$everything' do
@@ -66,20 +58,12 @@ RSpec.describe DEQMTestKit::PatientEverything do
     let(:test) { test_by_id(group, 'patient-everything-all-patients') }
 
     # multiple patient example for Patient/$everything
-    multiple_patient_file = File.open('./lib/fixtures/multiplePatientBundle.json')
-    multiple_patient_bundle = JSON.parse(multiple_patient_file.read).to_json
-    multiple_patient_response_file = File.open('./spec/fixtures/multiplePatientResponse.json')
-    multiple_patient_response = JSON.parse(multiple_patient_response_file.read).to_json
+    multiple_patient_bundle = JSON.parse(File.read('./lib/fixtures/multiplePatientBundle.json'))
+    multiple_patient_response = File.read('./spec/fixtures/multiplePatientResponse.json')
 
-    multiple_patient_length = multiple_patient_bundle.length
     mult_headers = {
       'Accept' => 'application/fhir+json',
-      'Accept-Charset' => 'utf-8',
-      'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      'Content-Length' => multiple_patient_length.to_s,
-      'Content-Type' => 'application/fhir+json',
-      'Host' => 'example.com',
-      'User-Agent' => 'Ruby FHIR Client'
+      'Content-Type' => 'application/fhir+json'
     }
 
     it 'passes for valid request to Patient/$everything' do
@@ -105,6 +89,7 @@ RSpec.describe DEQMTestKit::PatientEverything do
 
   describe 'Patient/<id>/$everything fails for invalid id' do
     let(:test) { test_by_id(group, 'patient-everything-invalid-patient-id') }
+    # these too
 
     it 'passes with correct Operation-Outcome returned' do
       stub_request(:post, "#{url}/Patient/INVALID/$everything")
